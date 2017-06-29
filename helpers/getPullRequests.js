1936e399d882d787786532e8b70aa3a97d411dcc
Dmitry Kunin <dkunin@avito.ru>
2017-06-29 16:52:35 +0300

    master:Testing stuff out

--- helpers/getPullRequests.js
+++ helpers/getPullRequests.js
@@ -5,8 +5,9 @@ module.exports = function(auth, project, host) {
     return new Promise(function(resolve, reject) {
         var options = {
             host,
+            // http://stash.msk.avito.ru/rest/api/latest/dashboard/pull-requests?state=open&role[]=author&role[]=REVIEWER&role[]=participant&limit=100
             path: (
-                '/rest/api/1.0/projects/' + project + '/pull-requests?limit=100'
+                '/rest/api/1.0/projects/' + project + '/pull-requests?limit=100&state=merged'
             ),
             headers: {
                 Authorization: 'Basic ' + auth,
---
+++ merged.js
@@ -0,0 +1,48 @@
+'use strict';
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const { processPrs, getRequests, simpleFlatten, cors } = require('./helpers');
+
+const PORT = 4848;
+const { JIRA_PASS, STASH_HOST, STASH_PROJECTS } = process.env;
+const PROJECTS = STASH_PROJECTS.split(' ').filter(Boolean);
+
+app.use(cors);
+
+function processArrayOfPrsAndConcat(arrayOfPrs, username) {
+    return simpleFlatten(
+        arrayOfPrs.map(singleResult =>
+            processPrs(
+                JSON.parse(singleResult).values,
+                JIRA_PASS,
+                STASH_HOST,
+                username
+            ))
+    );
+}
+
+function generateRequests(project) {
+    return getRequests(JIRA_PASS, project, STASH_HOST);
+}
+
+app.get('/api/prs', function(req, res) {
+    const { username } = req.query;
+    Promise.all(PROJECTS.map(generateRequests))
+        .then(function(result) {
+            Promise.all(
+                processArrayOfPrsAndConcat(result, username)
+            ).then(result => res.json(Array.isArray(result) ? result : []));
+        })
+        .catch(function(error) {
+            console.log(error);
+            res.send([]);
+        });
+});
+
+app.listen(PORT);
+
+console.log(
+    `Started service on port http://localhost:${PORT}/api/prs?username=YOUR_USERNAME`
+);
